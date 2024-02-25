0c8b88a3ca5dbc4d49b43592747e7c451d3036cd
pearlde-bink <tuanbinhnguyenngoc@gmail.com>
2024-02-25 22:43:09 +0700

    add some file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
 const app = express()
 const port = 3000
+const handlebars = require('express-handlebars')
+
+//http logger
+app.use(morgan('combined'))
+
+//template engine
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.render('home')
 })
 
 app.get('/trangchu', (req, res) => {
-    res.send('Trang chu em oi')
+    res.render('trangchu')
 })
 
 app.get('/tintuc', (req, res) => {
